00929e31531d2a2335ec1220654031ebea7047a5
Javier Fernández-Bravo Peñuela <fcojavier.fernandezbravo.uclm@gmail.com>
2023-07-25 19:20:19 +0200

    delay example

---
+++ target/delay.cpp
@@ -0,0 +1,15 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/thanatos/Documentos/lab/src/delay.ino"
+void setup();
+void loop();
+#line 1 "/home/thanatos/Documentos/lab/src/delay.ino"
+void setup() {
+}
+
+void loop() {
+    delay(500);
+}
