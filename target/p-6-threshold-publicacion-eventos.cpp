5705c5a0e899426e6057cf4283e8c58a20dccbf4
Javier Fernández-Bravo Peñuela <fcojavier.fernandezbravo.uclm@gmail.com>
2023-09-23 12:39:55 +0200

    P6 event publishing threshold assignment

---
+++ target/p-6-threshold-publicacion-eventos.cpp
@@ -0,0 +1,59 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/thanatos/Documentos/lab/src/p-6-threshold-publicacion-eventos.ino"
+void setup();
+void loop();
+#line 1 "/home/thanatos/Documentos/lab/src/p-6-threshold-publicacion-eventos.ino"
+#define LED_PIN A4
+
+short mano(String data);
+void turnLed(bool status);
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    mano("no_esta");
+
+    Particle.function("fraferp9_mano", mano);
+}
+
+void loop() {
+    delay(200);
+}
+
+/** Published function
+ *  If the string received equals si_esta/no_esta,
+ *  turn on/off the led and publish the matching event. */
+short mano(String data) {
+    short esta = -1;
+    if (data.length() == 7 && data.endsWith("_esta")) {
+        if (data.startsWith("si")) {
+            esta = 1;
+        } else if (data.startsWith("no")) {
+            esta = 0;
+        }
+    }
+
+    switch (esta) {
+        case 1:
+            turnLed(true);
+            break;
+        case 0:
+            turnLed(false);
+            break;
+    }
+
+    return esta;
+}
+
+void turnLed(bool status) {
+    if (status) {
+        digitalWrite(LED_PIN, HIGH);
+        Particle.publish("encendido");
+    } else {
+        digitalWrite(LED_PIN, LOW);
+        Particle.publish("apagado");
+    }
+}
